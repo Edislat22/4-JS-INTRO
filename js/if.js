302ee5c447872c66daacb9b6ebeba1f0bc3e6c49
Eduardas <latoanas@gmail.com>
2021-01-21 19:36:22 +0200

    pradzia 2

---
+++ js/if.js
@@ -0,0 +1,18 @@
+/*
+
+if (tikrinama salyga) {
+    jei tenkinama
+} else {
+    jei ne tenkinama
+}
+
+*/
+
+const a = 4;
+const b = 2;
+
+if (a < b) {
+    console.log('taip');
+} else {
+    console.log('ne');
+}
