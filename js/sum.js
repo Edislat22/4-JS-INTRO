47779ba293040d2d5eafbb9c32e8453f0b663f77
Eduardas <latoanas@gmail.com>
2021-01-25 13:44:13 +0200

    Sum.js

---
+++ js/sum.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const n1 = 2;
+const n2 = 5;
+
+const n3 = 22;
+const n4 = 55;
+
+const n5 = 222;
+const n6 = 555;
+
+function sum(firstNumber, secondNumber) {
+    const totalSum = firstNumber + secondNumber;
+    console.log(totalSum);
+}
+
+sum(9, 4);
+sum(n1, n2);
+sum(n3, n4);
+sum(n5, n6);
+
